1b379e266f95adc27d4a3a324bae86d951c6ca6a
Sebastian Fernandez <108418842+sebastianfer@users.noreply.github.com>
2023-03-13 19:33:41 -0400

    Update StepperMotor.py

--- StepperMotor.py
+++ StepperMotor.py
@@ -1,62 +1,59 @@
-# Import required libraries
 import sys
 import time
 import RPi.GPIO as GPIO
 
-# Use BCM GPIO references
-# instead of physical pin numbers
+# Use BCM GPIO references instead of physical pin numbers
 GPIO.setmode(GPIO.BOARD)
 
 # Define GPIO signals to use
-
 ControlPin = [12,11,13,15]
 
 # Set all pins as output
-
 for pin in ControlPin:
   GPIO.setup(pin,GPIO.OUT)
   GPIO.output(pin, 0)
 
+# Counter-clockwise
 ccw =  [[1,1,0,0],
        [0,1,1,0],
        [0,0,1,1],
        [1,0,0,1],
 ]
 
+# Clockwise
 cw = [[1,0,0,1],
-       [0,0,1,1],
-       [0,1,1,0],
        [1,1,0,0],
+       [0,1,1,0],
+       [0,0,1,1],
 ]
 
-rotationNeeded = 0
-rotationCount = 0
-
-while(1):
-    userInput = input("Press e-exit, Number of Rotations: ")
-    print("\n")
-    orientation = input("What orientation (cw or ccw): ")
-    print("\n")
-    if userInput == 'e':
-        break;
-
-    rotationNeeded = int(userInput)
-    rotationCount = 50 * rotationNeeded
-
+def MoveMotor(rotationCount, orientation):
     if orientation == 'cw':
-        for i in range(rotationCount):
-            for fullStep in range(4):
-                for pin in range(4):
-                    GPIO.output(ControlPin[pin], cw[fullStep][pin])
-                    time.sleep(0.001)
+       sequence = cw
+    elif orientation == 'ccw':
+       sequence = ccw
+    else:
+       print("Invalid orientation")
+       return
 
+    # Rotate Motor in a full Step
+    for i in range(rotationCount):
+       for fullStep in range(4):
+          for pin in range(4):
+             GPIO.output(ControlPin[pin], sequence[fullStep][pin])
+             time.sleep(0.001)
 
-    if orientation == 'ccw':
-        for i in range(rotationCount):
-            for fullStep in range(4):
-                for pin in range(4):
-                    GPIO.output(ControlPin[pin], ccw[fullStep][pin])
-                    time.sleep(0.001)
+while True:
+   userInput = input("Press e for exit or insert number of Rotations: ")
+   if userInput == 'e':
+      break
+   
+   # Spin the Motor
+   rotationCount = int(userInput) * 50
+   
+   # Define the orientation
+   orientation = input("Orientation (cw or ccw): ")
 
+   MoveMotor(rotationCount, orientation)
 
 GPIO.cleanup()
